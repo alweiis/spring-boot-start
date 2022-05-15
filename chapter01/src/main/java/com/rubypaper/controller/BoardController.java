b107be0375aee0218d9c27c781c25bfcca4ab648
alweiis <jstm1207@gmail.com>
2022-05-15 11:22:58 +0900

    feat: @RestController 기능 체크

--- chapter01/src/main/java/com/rubypaper/controller/BoardController.java
+++ chapter01/src/main/java/com/rubypaper/controller/BoardController.java
@@ -1,7 +1,12 @@
 package com.rubypaper.controller;
 
+import com.rubypaper.domain.BoardVO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @RestController
 public class BoardController {
@@ -13,5 +18,33 @@ public class BoardController {
     @GetMapping("/hello")
     public String hello(String name) {
         return "Hello : " + name;
+    }
+
+    @GetMapping("/getBoard")
+    public BoardVO getBoard() {
+        BoardVO board = new BoardVO();
+        board.setSeq(1);
+        board.setTitle("테스트 제목");
+        board.setWriter("테스터");
+        board.setContent("테스트 내용");
+        board.setCreateDate(new Date());
+        board.setCnt(0);
+        return board;
+    }
+
+    @GetMapping("/getBoardList")
+    public List<BoardVO> getBoardList() {
+        List<BoardVO> boardList = new ArrayList<>();
+        for (int i=1; i <= 10; i++) {
+            BoardVO board = new BoardVO();
+            board.setSeq(i);
+            board.setTitle("제목" + i);
+            board.setWriter("테스터");
+            board.setContent(i+ "번 테스트 내용입니다.");
+            board.setCreateDate(new Date());
+            board.setCnt(0);
+            boardList.add(board);
+        }
+        return boardList;
     }
 }
