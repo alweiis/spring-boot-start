fa9f428f559d740e8cb9ff884874ab1c7a99f71e
alweiis <jstm1207@gmail.com>
2022-05-28 18:03:38 +0900

    feat: CRUD 기능 테스트(상세 조회)

--- chapter05/src/test/java/com/rubypaper/BoardRepositoryTest.java
+++ chapter05/src/test/java/com/rubypaper/BoardRepositoryTest.java
@@ -14,7 +14,7 @@ public class BoardRepositoryTest {
     @Autowired
     private BoardRepository boardRepo;
 
-    @Test
+    //@Test
     public void testInsertBoard() {
         Board board = new Board();
         board.setTitle("첫 번째 게시글");
@@ -24,5 +24,11 @@ public class BoardRepositoryTest {
         board.setCnt(0L);
 
         boardRepo.save(board);
+    }
+
+    @Test
+    public void testGetBoard() {
+        Board board = boardRepo.findById(1L).get();
+        System.out.println(board);
     }
 }
